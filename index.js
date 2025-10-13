e13dc9035408bfe9631db3d47421ba1bdc92591e
foued <fouedsaidane2@gmail.com>
2025-10-13 09:00:23 +0200

    Update CORS configuration to use the correct Vercel deployment URL

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", // ton front local
-      "https://tonsite.vercel.app", // ton site déployé sur Vercel
+      "https://talyet-ujne.vercel.app/", // ton site déployé sur Vercel
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
